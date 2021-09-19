eee23fbf7b6c44488a729dde4d4cbc6feb7ab8ca
jimeshshah <jimeshcool007@gmail.com>
2021-09-19 11:29:11 +0530

    Production demo changes

--- src/main/java/com/example/springtokendemo/SpringTokenDemoApplication.java
+++ src/main/java/com/example/springtokendemo/SpringTokenDemoApplication.java
@@ -2,9 +2,16 @@ package com.example.springtokendemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringTokenDemoApplication {
+public class SpringTokenDemoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringTokenDemoApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringTokenDemoApplication.class, args);
