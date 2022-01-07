efa62a6a00e7c362af0797ef0a1cdb0da51e8578
jimeshshah <jimeshcool007@gmail.com>
2022-01-07 22:41:04 +0530

    Added changes

--- scanner-front/src/api.js
+++ scanner-front/src/api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default axios.create({
     // baseURL: 'http://localhost:8080/'
-    baseURL: 'https://srfib.com/LicenseServer/'
+    baseURL: 'https://test.srfib.com'
     // baseURL: 'https://1ed1-2402-3a80-131e-6505-986e-2736-c060-1412.ngrok.io/' 
 });
--- src/main/java/com/example/springtokendemo/service/LicenseService.java
+++ src/main/java/com/example/springtokendemo/service/LicenseService.java
@@ -4,6 +4,10 @@ import com.example.springtokendemo.model.BlockedLicenses;
 import com.example.springtokendemo.model.dto.DriverLicense;
 import com.example.springtokendemo.model.dto.LicenseResponseDto;
 import com.example.springtokendemo.repository.LicenseRepo;
+import com.example.springtokendemo.security.jwt.AuthTokenFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,8 +16,12 @@ import java.util.Optional;
 @Service
 public class LicenseService {
 
+    private static final Logger logger = LoggerFactory.getLogger(LicenseService.class);
+
     private final LicenseRepo licenseRepo;
     private final IdScanService idScanService;
+
+
 
     public LicenseService(LicenseRepo licenseRepo, IdScanService idScanService) {
         this.licenseRepo = licenseRepo;
@@ -33,14 +41,21 @@ public class LicenseService {
     }
 
     public LicenseResponseDto parseText(String text) throws Exception {
+        logger.info("Parse Text :"+ text+ ":");
+
         DriverLicense license = idScanService.getLicense(text);
 
-        Optional<BlockedLicenses> blockedLicensesOptional = licenseRepo.findByLicenseIgnoreCase(license.getLicenseNumber());
-        if (blockedLicensesOptional.isPresent()) {
-            BlockedLicenses blockedLicenses = blockedLicensesOptional.get();
-            return new LicenseResponseDto(license.getFullName(), license.getLicenseNumber(), license.getAddress1(),
-                    license.getAddress2(), license.getCountry(), license.getCity(), license.getPostalCode(), true, blockedLicenses.getId(), blockedLicenses.getReason());
+        if(license != null) {
+            logger.info("License DTO parsed: "+ license);
+            Optional<BlockedLicenses> blockedLicensesOptional = licenseRepo.findByLicenseIgnoreCase(license.getLicenseNumber());
+            if (blockedLicensesOptional.isPresent()) {
+                BlockedLicenses blockedLicenses = blockedLicensesOptional.get();
+                logger.info("License is blocked : "+blockedLicenses);
+                return new LicenseResponseDto(license.getFullName(), license.getLicenseNumber(), license.getAddress1(),
+                        license.getAddress2(), license.getCountry(), license.getCity(), license.getPostalCode(), true, blockedLicenses.getId(), blockedLicenses.getReason());
+            }
         }
+        logger.info("License is not blocked");
         return new LicenseResponseDto(license.getFullName(), license.getLicenseNumber(), license.getAddress1(),
                 license.getAddress2(), license.getCountry(), license.getCity(), license.getPostalCode(), false, null, null);
     }
